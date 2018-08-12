7597d374af133e8ec2e159bedf703bdac7603bc8
hgryoo <hyunggyu.ryoo@gmail.com>
2018-08-13 04:24:37 +0900

    release 1 - polyline done

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,9 +11,9 @@ sudo apt-get install tomcat8 -y
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt xenial-pgdg main" >> /etc/apt/sources.list'
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 
-sudo apt-get install postgresql-10 -y
-sudo apt-get install postgresql-10-postgis-2.4 -y
-sudo apt-get install postgresql-10-postgis-scripts -y
+sudo apt-get install postgresql-9.6 -y
+sudo apt-get install postgresql-9.6-postgis-2.3 -y
+sudo apt-get install postgresql-9.6-postgis-scripts -y
 sudo apt-get install postgis -y
 
 # configure PostGIS
@@ -22,34 +22,33 @@ sudo -u postgres psql -f /vagrant/db.gen
 sudo apt-get install build-essential g++ cmake openscenegraph libopenscenegraph-dev libgmp3-dev libmpfr-dev -y
 sudo apt-get install maven -y
 
-# 코드로부터 빌드
+cd ~/
 git clone https://github.com/hgryoo/SFCGAL4J.git
 # cd ~/SFCGAL4J/sfcgal4j-impl
 # ./cppbuild.sh install
 # cd ../
 cd ~/SFCGAL4J
-mvn clean install -U -DskipTests
-cd ~/
+mvn clean install -DskipTests
 
+cd ~/
 git clone https://github.com/STEMLab/geotools-3d-extension.git
 cd ~/geotools-3d-extension
-mvn clean install -U -DskipTests
-cd ~/
+mvn clean install -DskipTests
 
+cd ~/
 git clone https://github.com/STEMLab/geoserver-3d-extension.git
 cd ~/geoserver-3d-extension
-mvn clean install -U -DskipTests
-cd ~/
+mvn clean install -DskipTests
 
+cd ~/
 git clone https://github.com/STEMLab/geoserver.git
 cd ~/geoserver
 git checkout geoserver3d
 cd src
-mvn clean install -U -DskipTests
+mvn clean install -DskipTests
 
 cd web/app
-mvn clean install -U -DskipTests
-cd ~/
+mvn clean install -DskipTests
 
 # deploy GeoServer
 cd ~/geoserver/src/web/app/target
@@ -58,10 +57,9 @@ sudo service tomcat8 restart
 
 cd ~/
 git clone https://github.com/hgryoo/SimpleWFSClient4Mago3D.git
-cp -rf ~/SimpleWFSClient4Mago3D/Mago3D/* ~/mago3djs/
 cd ~/SimpleWFSClient4Mago3D
 #PGPASSWORD=postgis pg_restore --clean -h localhost -p 5432 -U postgis -d gisdb -v ./buildings.dump
-
+sudo -u postgres PGPASSWORD=postgres pg_restore -U -i -h localhost -p 5432 -U postgres -d gisdb -v ./buildings.dump
 curl -sL https://deb.nodesource.com/setup_10.x| sudo -E bash -
 sudo apt-get install -y nodejs 
 sudo apt-get install npm -y
@@ -71,9 +69,14 @@ git clone https://github.com/Gaia3D/mago3djs.git
 cd ~/mago3djs
 git checkout develop
 
-sudo npm install
+cp -rf ~/SimpleWFSClient4Mago3D/Mago3D/* ~/mago3djs/
+
+npm install
 sudo npm i -g gulp
 gulp
+
+npm install restler --save
+npm install body-parser --save
 
 #Install PM2
 sudo npm install -g pm2
